8c6f920db4576cdfcb7418802f32e05511491803
turnerj20 <turnerj20@vcu.edu>
2025-09-10 17:14:38 -0400

    Query hotel data based on input (tool for LangGraph)

--- agent.py
+++ agent.py
@@ -6,13 +6,18 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langchain_core.chat_history import InMemoryChatMessageHistory
 
-
+#Langgraph
+from langgraph.graph import StateGraph, START
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import tools_condition
 
 load_dotenv()
 
 #Load csv into df and only keep necessary columns
-hotels = pd.read_csv("hotels.csv")
-hotels = hotels.drop(["location_base","staff_base","value_for_money_base"],axis=1)
+@st.cache_resource
+def get_csv():
+    """Load hotel data from CSV file"""
+    return pd.read_csv("hotels.csv").drop(["location_base","staff_base","value_for_money_base"],axis=1)
 st.set_page_config(page_title="Hotel QA Agent", layout="centered")
 
 def initialize_session_state():
@@ -32,6 +37,41 @@ def get_model():
         st.stop()
     return ChatOpenAI(model="gpt-4o-mini",temperature=0.6,openai_api_key=api_key)
 
+
+
+def query_hotels(city: str=None, country: str=None, star_rating: int=None, cleanliness: int=None, 
+                 comfort: int=None, facilities: int=None, sort_by: str=None, num_results: int=None) -> pd.DataFrame:
+    """Queries hotels based on provided criteria. 
+    city, country (case-insensitive string filters),
+    minimum thresholds for star rating, cleanliness, comfort, and facilities (using parameters star_rating, cleanliness, comfort, facilities),
+    sorting by a selected column (e.g., star rating, cleanliness, comfort, facilities using ),
+    limiting the number of results returned
+    Returns: a DataFrame of hotels matching the criteria.
+    """
+
+    csv = get_csv()
+    if city:
+        csv = csv[csv['city'].str.lower() == city.lower()]
+    if country:
+        csv = csv[csv['country'].str.lower() == country.lower()]
+    if star_rating:
+        csv = csv[csv['star_rating'] >= star_rating]
+    if cleanliness:
+        csv = csv[csv['cleanliness_base'] >= cleanliness]
+    if comfort:
+        csv = csv[csv['comfort_base'] >= comfort]
+    if facilities:
+        csv = csv[csv['facilities_base'] >= facilities]
+    if sort_by:
+        csv = csv.sort_values(by=sort_by, ascending=False)
+    if num_results:
+        csv = csv.head(num_results)
+    return csv
+
+
+
+
+
 def main():
     st.title("Hotel QA Agent")
     st.markdown("---")
@@ -41,4 +81,5 @@ def main():
         st.error("Please set the OPENAI_API_KEY environment variable.")
         st.stop()
 
-        
+if __name__ == "__main__":
+    main()   
